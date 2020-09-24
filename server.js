07c8081a0c5859920fd4e6ac64459516fd30836d
lakshygupta <lakshy.8192211@gmail.com>
2020-09-24 19:38:21 +0530

    second commit 24-9

--- server.js
+++ server.js
@@ -18,39 +18,53 @@ if(process.env.NODE_ENV === 'production'){
   }
 
 // const io = socket(server);
-
+const users = {};
+// var connectCounter = 0;
 io.sockets.on('connection',
   // We are given a websocket object in our function
   function (socket) {
   
     console.log("We have a new client: " + socket.id);
+    socket.broadcast.emit('recieve',  io.sockets.server.engine.clientsCount);
+    // socket.broadcast.emit('recieve',  connectCounter);
   
     // When this user emits, client side: socket.emit('otherevent',some data);
     socket.on('mouse',
       function(data) {
         // Data comes in as whatever was sent, including objects
         console.log("Received: 'mouse' " + data.x + " " + data.y);
-      
+        
         // Send it to all other clients
         socket.broadcast.emit('mouse', data);
+        socket.broadcast.emit('recieve',  io.sockets.server.engine.clientsCount);
         
         
         // This is a way to send to everyone including sender
         // io.sockets.emit('message', "this goes to everyone");
-
+        
       }
-    );
+      );
+      
+      // socket.on('users', () => {
+        //     socket.broadcast.emit('users', () => { console.log(io.sockets.server.engine.clientsCount); })
+        // });
+        
+        // socket.on('connect', function() { connectCounter++; });
+        
+        socket.on('disconnect', function() {
+          // connectCounter--;
+          console.log("Client has disconnected");
+          socket.broadcast.emit('left', users[socket.id]);
+          delete users[socket.id];
+        });
+        
+        
+        socket.on('new-user-joined', name => {
+          console.log("New User ", name);
+          users[socket.id] = name;
+          socket.broadcast.emit('user-joined', name);
+        });
 
-    // socket.on('users', () => {
-    //     socket.broadcast.emit('users', () => { console.log(io.sockets.server.engine.clientsCount); })
-    // });
 
-    socket.on('users', message => {
-        socket.broadcast.emit('recieve', {message: io.sockets.server.engine.clientsCount})
-    });
-    
-    socket.on('disconnect', function() {
-      console.log("Client has disconnected");
-    });
   }
 );
